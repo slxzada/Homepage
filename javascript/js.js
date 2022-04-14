eace9932c193a0ae811851c95fc9cc156fc197f7
slxzada <99103642+slxzada@users.noreply.github.com>
2022-04-13 22:08:38 -0400

    Add files via upload

--- javascript/js.js
+++ javascript/js.js
@@ -1,52 +1,38 @@
-$(document).click(function(){
-    $(".grid-container").css("grid-template-columns", "repeat(8, 1fr)")
-})
-
 $(document).mousemove(function(){
     if ($(".hello:hover").length == 1){
-        $(".hello").css("color", "rgb(240, 226, 108)")
+        $(".hello").css("color", "white")
     }
     else {
-        $(".hello").css("color", "rgb(143, 189, 144)")
+        $(".hello").css("color", "rgb(130, 205, 218)")
     }
 })
 
 $(document).mousemove(function(){
-    if ($(".item1:hover").length == 1){
-        $(".item1").css("background", "rgb(240, 226, 108)")
+    if ($(".introduction-1:hover").length == 1){
+        $(".introduction-1").css("color", "rgb(130, 205, 218)")
     }
     else {
-        $(".item1").css("background", "none")
-    }
-
-})
-
-$(document).mousemove(function(){
-    if ($(".item2:hover").length == 1){
-        $(".item2").css("background", "rgb(240, 226, 108)")
-    }
-    else {
-        $(".item2").css("background", "none")
+        $(".introduction-1").css("color", "white")
     }
 
 })
 
 $(document).mousemove(function(){
-    if ($(".item3:hover").length == 1){
-        $(".item3").css("background", "rgb(240, 226, 108)")
+    if ($(".introduction-2:hover").length == 1){
+        $(".introduction-2").css("color", "rgb(130, 205, 218)")
     }
     else {
-        $(".item3").css("background", "none")
+        $(".introduction-2").css("color", "white")
     }
 
 })
 
 $(document).mousemove(function(){
-    if ($(".item4:hover").length == 1){
-        $(".item4").css("background", "rgb(240, 226, 108)")
+    if ($(".introduction-3:hover").length == 1){
+        $(".introduction-3").css("color", "rgb(130, 205, 218)")
     }
     else {
-        $(".item4").css("background", "none")
+        $(".introduction-3").css("color", "white")
     }
 
 })
@@ -77,4 +63,5 @@ $(document).mousemove(function(){
         $(".introduction-3").css("filter", "blur(5px)")
     }
 })
+
 
