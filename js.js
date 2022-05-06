dcf09526abdb94cb50b523fc57694e2bbca71b0e
slxzada <99103642+slxzada@users.noreply.github.com>
2022-05-05 22:57:23 -0400

    Add files via upload

--- js.js
+++ js.js
@@ -1,80 +1,84 @@
-$(document).click(function(){
-    $(".grid-container").css("grid-template-columns", "repeat(8, 1fr)")
-})
-
 $(document).mousemove(function(){
-    if ($(".hello:hover").length == 1){
-        $(".hello").css("color", "rgb(240, 226, 108)")
+    if ($("#hello:hover").length == 1){
+        $("#hello").css("color", "white")
     }
     else {
-        $(".hello").css("color", "rgb(143, 189, 144)")
+        $("#hello").css("color", "rgb(130, 205, 218)")
     }
 })
 
 $(document).mousemove(function(){
-    if ($(".item1:hover").length == 1){
-        $(".item1").css("background", "rgb(240, 226, 108)")
+    if ($("#introduction-1:hover").length == 1){
+        $("#introduction-1").css("color", "rgb(130, 205, 218)")
     }
     else {
-        $(".item1").css("background", "none")
+        $("#introduction-1").css("color", "white")
     }
 
 })
 
 $(document).mousemove(function(){
-    if ($(".item2:hover").length == 1){
-        $(".item2").css("background", "rgb(240, 226, 108)")
+    if ($("#introduction-2:hover").length == 1){
+        $("#introduction-2").css("color", "rgb(130, 205, 218)")
     }
     else {
-        $(".item2").css("background", "none")
+        $("#introduction-2").css("color", "white")
     }
 
 })
 
 $(document).mousemove(function(){
-    if ($(".item3:hover").length == 1){
-        $(".item3").css("background", "rgb(240, 226, 108)")
+    if ($("#introduction-3:hover").length == 1){
+        $("#introduction-3").css("color", "rgb(130, 205, 218)")
     }
     else {
-        $(".item3").css("background", "none")
+        $("#introduction-3").css("color", "white")
     }
 
 })
 
 $(document).mousemove(function(){
-    if ($(".item4:hover").length == 1){
-        $(".item4").css("background", "rgb(240, 226, 108)")
+    if ($("#introduction-4:hover").length == 1){
+        $("#introduction-4").css("color", "rgb(130, 205, 218)")
     }
     else {
-        $(".item4").css("background", "none")
+        $("#introduction-4").css("color", "white")
     }
 
 })
 
 $(document).mousemove(function(){
-    if ($(".introduction-1:hover").length ==1){
-        $(".introduction-1").css("filter", "none")
+    if ($("#introduction-1:hover").length ==1){
+        $("#introduction-1").css("filter", "none")
     }
     else {
-        $(".introduction-1").css("filter", "blur(5px)")
+        $("#introduction-1").css("filter", "blur(5px)")
     }
 })
 
 $(document).mousemove(function(){
-    if ($(".introduction-2:hover").length ==1){
-        $(".introduction-2").css("filter", "none")
+    if ($("#introduction-2:hover").length ==1){
+        $("#introduction-2").css("filter", "none")
     }
     else {
-        $(".introduction-2").css("filter", "blur(5px)")
+        $("#introduction-2").css("filter", "blur(5px)")
     }
 })
 
 $(document).mousemove(function(){
-    if ($(".introduction-3:hover").length ==1){
-        $(".introduction-3").css("filter", "none")
+    if ($("#introduction-3:hover").length ==1){
+        $("#introduction-3").css("filter", "none")
     }
     else {
-        $(".introduction-3").css("filter", "blur(5px)")
+        $("#introduction-3").css("filter", "blur(5px)")
     }
 })
 
+$(document).mousemove(function(){
+    if ($("#introduction-4:hover").length ==1){
+        $("#introduction-4").css("filter", "none")
+    }
+    else {
+        $("#introduction-4").css("filter", "blur(5px)")
+    }
+})
